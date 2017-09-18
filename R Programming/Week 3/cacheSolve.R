c907b246accd253e2cf626b5aed002ca1738e65e
Benjamin Hunt <benjamintyhunt@gmail.com>
2017-09-18 15:12:27 -0700

    Create cacheSolve.R

---
+++ R Programming/Week 3/cacheSolve.R
@@ -0,0 +1,12 @@
+## Just had to change a few things here
+cacheSolve <- function(x, ...) {
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
+}
